4cc2ed156fa06b73ea0c953d483234bf90514e16
mih777 <mih777777@yandex.ru>
2020-01-21 00:38:40 +0300

    claster added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,42 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import os = require('os')
+import * as cluster  from 'cluster'
 
-async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule)
-  app.enableCors()
-  const port = process.env.PORT || 3000
-  await app.listen(port)
+const pid = process.pid
 
+if(cluster.isMaster) {
+    const cpusCount = os.cpus().length 
+    console.log(`CPUs: ${cpusCount}`)
+    console.log(`Master started. Pid: ${pid}`)
+    for(let i = 0; i < cpusCount-1; i++) {
+        const worker = cluster.fork()
+        worker.on('exit', () => {
+            console.log(`Worker died! Pid: ${worker.process.pid}`)
+            cluster.fork()
+        })
+        worker.send('Hello frm server')
+        worker.on('message', (msg) => {
+            console.log(`Message from worker ${worker.process.pid} : ${JSON.stringify(msg)}`)
+        })
+    }
 }
 
-bootstrap();
+if(cluster.isWorker) {
+  async function bootstrap() {
+
+    const app = await NestFactory.create(AppModule)
+    app.enableCors()
+    const port = process.env.PORT || 3000
+    await app.listen(port)
+  
+  }
+  
+  bootstrap();
+    process.on('message', (msg) => {
+        console.log(`Messsage from Master: ${msg}`)
+    })
+    process.send({text: 'Hello', pid})
+}
+
